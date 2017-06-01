9e8016137539b77e389d0f45d4949e3487908176
chadina <dina_1_96@hotmail.com>
2017-06-01 12:54:36 +0200

    Add files via upload

--- Besökare.java
+++ Besökare.java
@@ -1,25 +1,31 @@
 package Festival;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class Besökare extends JPanel {
+public class Besökare extends JPanel  {
 	
 	private JPanel panel = new JPanel();
+	private JPanel bandpanel = new JPanel(); 
 	private JLabel spelschema = new JLabel("Spelschema");
 	private JLabel bandnamn = new JLabel("Band");
-	private JLabel vart = new JLabel("Plats");
+	private JLabel vart = new JLabel("Scen");
 	private JLabel tid = new JLabel("Tid");
-	private JLabel bandinfo = new JLabel("Bandinfo");
-	private JLabel kontakt = new JLabel("Kontaktperson");
+//	private JLabel bandinfo = new JLabel("Bandinfo");
+//	private JLabel kontakt = new JLabel("Kontaktperson");
 	private JLabel info = new JLabel("Skriv in det band du vill ha mer info ifrån här:");
 	private JLabel sceninfo = new JLabel("Välj ifrån listan vilken scen du vill ha mer info ifrån:");
 	private JTextArea textarea = new JTextArea("");
 	private JTextArea bandtext = new JTextArea("");
 	private JButton klar = new JButton("Fortsätt");
 	private JComboBox box = new JComboBox();
+	
+	bListener listener = new bListener();
+	private String bandinfo;
 	
 	
 	public Besökare() {
@@ -41,8 +47,8 @@ public class Besökare extends JPanel {
 		this.add(bandnamn);
 		this.add(vart);
 		this.add(tid);
-		this.add(bandinfo);
-		this.add(kontakt);
+//		this.add(bandinfo);
+//		this.add(kontakt);
 		this.add(textarea);
 		this.add(bandtext);
 		this.add(info);
@@ -56,10 +62,10 @@ public class Besökare extends JPanel {
 		spelschema.setFont( new Font("Comic Sans MS", Font.BOLD, 20) );
 		spelschema.setBounds(260, 10, 150, 50);
 		bandnamn.setBounds(40,60, 150, 40);
-		vart.setBounds(160,60, 150, 40);
-		bandinfo.setBounds(280,60, 150, 40);
-		tid.setBounds(400,60, 150, 40);
-		kontakt.setBounds(480,60, 150, 40);
+		vart.setBounds(250,60, 150, 40);
+//		bandinfo.setBounds(280,60, 150, 40);
+		tid.setBounds(440,60, 150, 40);
+//		kontakt.setBounds(480,60, 150, 40);
 		textarea.setBounds(40, 90, 530, 300);
 		bandtext.setBounds(40, 500, 250, 20);
 		info.setBounds(40, 470, 480, 40);
@@ -69,9 +75,9 @@ public class Besökare extends JPanel {
 
 	
 		panel.add(spelschema);
-		panel.add(bandinfo);
+//		panel.add(bandinfo);
 		panel.add(bandnamn);
-		panel.add(kontakt);
+//		panel.add(kontakt);
 		panel.add(tid);
 		panel.add(vart);
 		panel.add(textarea);
@@ -81,15 +87,50 @@ public class Besökare extends JPanel {
 		panel.add(sceninfo);
 		panel.add(box);
 		
-				
+			klar.addActionListener(listener);
+			
 		
 	}
 	
+	public Besökare (String bandinfo) {
+		
+		this.bandinfo=bandinfo;
+		
+		bandpanel.setPreferredSize(new Dimension(300, 300));
+		bandpanel.setBackground(Color.WHITE);
+		
+		this.add(bandpanel);
+		
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(300, 300);
+		frame.add(bandpanel);
+		frame.setLocationRelativeTo(null);
+	}
 	
+	
+	private class bListener implements ActionListener {
+		Besökare be;
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			if (arg0.getSource()==klar){
+				be = new Besökare ("hej");
+				
+				
+			}
+			
+			
+			
+		}
+		
+	}
 
 
+	
 	public static void main (String[] args) {
 		Besökare bes = new Besökare();
+//		Besökare b = new Besökare("linking park");
 		JFrame ja = new JFrame();
 		ja.setVisible(true);
 		ja.setSize(600, 600);
@@ -97,5 +138,6 @@ public class Besökare extends JPanel {
 		ja.setLocationRelativeTo(null);
 		
 	}
+	
 
 }
